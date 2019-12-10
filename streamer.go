1fe52ea22b38af9b4eaae56bbe05d0cf2fdab798
dc0d <kaveh.shahbazian@gmail.com>
2019-12-10 22:27:22 +0100

    move rest

--- streamer.go
+++ streamer.go
@@ -3,6 +3,7 @@ package streamer
 type SliceIterator struct {
 	input   []interface{}
 	current int
+	item    interface{}
 }
 
 func NewSliceIterator(input ...interface{}) *SliceIterator {
@@ -15,20 +16,28 @@ func NewSliceIterator(input ...interface{}) *SliceIterator {
 }
 
 func (it *SliceIterator) Next() bool {
+	if it.item != nil {
+		return true
+	}
+
 	if it.current+1 < len(it.input) {
 		it.current++
+		it.item = it.input[it.current]
 		return true
 	}
 	return false
 }
 
-func (it *SliceIterator) Value() interface{} { return it.input[it.current] }
+func (it *SliceIterator) Value() interface{} {
+	item := it.item
+	it.item = nil
+	return item
+}
 
 //
 
 type Stream struct {
-	input    Iterator
-	iterator Iterator
+	input Iterator
 }
 
 func NewStream(input Iterator) (res *Stream) {
@@ -36,28 +45,168 @@ func NewStream(input Iterator) (res *Stream) {
 	return
 }
 
-func (st *Stream) Next() bool { return st.iterator.Next() }
+func (st *Stream) Next() bool { return st.input.Next() }
 
-func (st *Stream) Value() interface{} { return st.iterator.Value() }
+func (st *Stream) Value() interface{} { return st.input.Value() }
 
-func (st *Stream) Map(mapFn func(x interface{}) interface{}) {
-	st.iterator = newMapperStream(st.input, mapFn)
+func (st *Stream) Map(mapFn func(x interface{}) interface{}) *Stream {
+	iterator := newMapperStream(st.input, mapFn)
+	return NewStream(iterator)
 }
 
-func (st *Stream) ChunkBy(chunkFn func(x interface{}) interface{}) {
-	st.iterator = newChunkByStream(st.input, chunkFn)
+func (st *Stream) ChunkBy(chunkFn func(x interface{}) interface{}) *Stream {
+	iterator := newChunkByStream(st.input, chunkFn)
+	return NewStream(iterator)
 }
 
-func (st *Stream) ChunkEvery(chunkSize int) {
-	st.iterator = newChunkEveryStrem(st.input, chunkSize)
+func (st *Stream) ChunkEvery(chunkSize int) *Stream {
+	iterator := newChunkEveryStrem(st.input, chunkSize)
+	return NewStream(iterator)
 }
 
-func (st *Stream) Skip(skipCount int) {
-	st.iterator = newSkipCountStream(st.input, skipCount)
+func (st *Stream) Skip(skipCount int) *Stream {
+	iterator := newSkipCountStream(st.input, skipCount)
+	return NewStream(iterator)
 }
 
-func (st *Stream) SkipWhile(skipFn func(interface{}) bool) {
-	st.iterator = newSkipWhileStream(st.input, skipFn)
+func (st *Stream) SkipWhile(skipFn func(interface{}) bool) *Stream {
+	iterator := newSkipWhileStream(st.input, skipFn)
+	return NewStream(iterator)
+}
+
+func (st *Stream) Filter(filterFn func(interface{}) bool) *Stream {
+	iterator := newFilterStream(st.input, filterFn)
+	return NewStream(iterator)
+}
+
+func (st *Stream) Take(takeCount int) *Stream {
+	iterator := newTakeStream(st.input, takeCount)
+	return NewStream(iterator)
+}
+
+func (st *Stream) TakeWhile(takeFn func(interface{}) bool) *Stream {
+	iterator := newTakeWhileStream(st.input, takeFn)
+	return NewStream(iterator)
+}
+
+//
+
+type takeWhileStream struct {
+	input  Iterator
+	takeFn func(interface{}) bool
+
+	item  interface{}
+	taken bool
+}
+
+func newTakeWhileStream(input Iterator, takeFn func(interface{}) bool) (res *takeWhileStream) {
+	res = &takeWhileStream{
+		input:  input,
+		takeFn: takeFn,
+	}
+	return
+}
+
+func (tw *takeWhileStream) Next() bool {
+	if tw.taken {
+		return false
+	}
+	if tw.item != nil {
+		return true
+	}
+	next := tw.input.Next()
+	if !next {
+		return false
+	}
+	item := tw.input.Value()
+	if tw.takeFn(item) {
+		tw.item = item
+		return true
+	}
+	tw.taken = true
+	return false
+}
+
+func (tw *takeWhileStream) Value() interface{} {
+	value := tw.item
+	tw.item = nil
+	return value
+}
+
+//
+
+type takeStream struct {
+	input     Iterator
+	takeCount int
+
+	item interface{}
+}
+
+func newTakeStream(input Iterator, takeCount int) (res *takeStream) {
+	res = &takeStream{
+		input:     input,
+		takeCount: takeCount,
+	}
+	return
+}
+
+func (tc *takeStream) Next() bool {
+	if tc.item != nil {
+		return true
+	}
+	if tc.takeCount == 0 {
+		return false
+	}
+	next := tc.input.Next()
+	if !next {
+		return false
+	}
+	tc.item = tc.input.Value()
+	tc.takeCount--
+	return true
+}
+
+func (tc *takeStream) Value() interface{} {
+	value := tc.item
+	tc.item = nil
+	return value
+}
+
+//
+
+type filterStream struct {
+	input    Iterator
+	filterFn func(interface{}) bool
+
+	item interface{}
+}
+
+func newFilterStream(input Iterator, filterFn func(interface{}) bool) (res *filterStream) {
+	res = &filterStream{
+		input:    input,
+		filterFn: filterFn,
+	}
+	return
+}
+
+func (fs *filterStream) Next() bool {
+	if fs.item != nil {
+		return true
+	}
+	for fs.input.Next() {
+		item := fs.input.Value()
+		if fs.filterFn(item) {
+			fs.item = item
+			return true
+		}
+	}
+	return false
+}
+
+func (fs *filterStream) Value() interface{} {
+	value := fs.item
+	fs.item = nil
+	return value
 }
 
 //
@@ -109,8 +258,6 @@ func (sw *skipWhileStream) Value() interface{} {
 type skipCountStream struct {
 	input     Iterator
 	skipCount int
-
-	skipped bool
 }
 
 func newSkipCountStream(input Iterator, skipCount int) (res *skipCountStream) {
@@ -122,11 +269,11 @@ func newSkipCountStream(input Iterator, skipCount int) (res *skipCountStream) {
 }
 
 func (sc *skipCountStream) Next() bool {
-	if !sc.skipped {
+	if sc.skipCount != 0 {
 		for i := 0; i < sc.skipCount && sc.input.Next(); i++ {
 			sc.input.Value()
 		}
-		sc.skipped = true
+		sc.skipCount = 0
 	}
 	return sc.input.Next()
 }
@@ -151,7 +298,10 @@ func newChunkEveryStrem(input Iterator, chunkSize int) (res *chunkEveryStrem) {
 }
 
 func (ce *chunkEveryStrem) Next() bool {
-	ce.lastChunk = nil
+	if ce.lastChunk != nil {
+		return true
+	}
+
 	for ce.input.Next() {
 		ce.lastChunk = append(ce.lastChunk, ce.input.Value())
 		if len(ce.lastChunk) == ce.chunkSize {
@@ -161,7 +311,11 @@ func (ce *chunkEveryStrem) Next() bool {
 	return len(ce.lastChunk) > 0
 }
 
-func (ce *chunkEveryStrem) Value() interface{} { return ce.lastChunk }
+func (ce *chunkEveryStrem) Value() interface{} {
+	item := ce.lastChunk
+	ce.lastChunk = nil
+	return item
+}
 
 //
 
@@ -183,6 +337,10 @@ func newChunkByStream(input Iterator, chunkFn func(x interface{}) interface{}) (
 }
 
 func (cs *chunkByStream) Next() bool {
+	if cs.lastChunk != nil {
+		return true
+	}
+
 	var (
 		flag           interface{}
 		flagCalculated bool
@@ -194,7 +352,6 @@ func (cs *chunkByStream) Next() bool {
 		cs.lastFlag = nil
 	}
 
-	cs.lastChunk = nil
 	if cs.leftoverChunkItem != nil {
 		cs.lastChunk = append(cs.lastChunk, cs.leftoverChunkItem)
 		cs.leftoverChunkItem = nil
@@ -222,13 +379,19 @@ func (cs *chunkByStream) Next() bool {
 	return len(cs.lastChunk) > 0
 }
 
-func (cs *chunkByStream) Value() interface{} { return cs.lastChunk }
+func (cs *chunkByStream) Value() interface{} {
+	item := cs.lastChunk
+	cs.lastChunk = nil
+	return item
+}
 
 //
 
 type mapperStream struct {
 	input Iterator
 	mapFn func(x interface{}) interface{}
+
+	item interface{}
 }
 
 func newMapperStream(input Iterator, mapFn func(x interface{}) interface{}) (res *mapperStream) {
@@ -239,8 +402,22 @@ func newMapperStream(input Iterator, mapFn func(x interface{}) interface{}) (res
 	return
 }
 
-func (ms *mapperStream) Next() bool         { return ms.input.Next() }
-func (ms *mapperStream) Value() interface{} { return ms.mapFn(ms.input.Value()) }
+func (ms *mapperStream) Next() bool {
+	if ms.item != nil {
+		return true
+	}
+	next := ms.input.Next()
+	if next {
+		ms.item = ms.mapFn(ms.input.Value())
+	}
+	return next
+}
+
+func (ms *mapperStream) Value() interface{} {
+	item := ms.item
+	ms.item = nil
+	return item
+}
 
 //
 
--- streamer_test.go
+++ streamer_test.go
@@ -48,6 +48,30 @@ func Test_slice_iterator(t *testing.T) {
 			assert.Equal(len(expectedOutput), index)
 		})
 	}
+
+	{
+		var (
+			input          = []interface{}{1, 2, "3"}
+			expectedOutput = []interface{}{1, 2, "3"}
+		)
+
+		t.Run("slice iterator does not proceed on Next() until the last value is read by Value()", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+			)
+
+			index := 0
+			for iterator.Next() {
+				iterator.Next()
+				assert.Equal(expectedOutput[index], iterator.Value())
+				index++
+			}
+
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
 }
 
 func Test_stream_map(t *testing.T) {
@@ -83,10 +107,38 @@ func Test_stream_map(t *testing.T) {
 				stream                     = streamer.NewStream(iterator)
 			)
 
-			stream.Map(mapFn)
+			stream = stream.Map(mapFn)
 
 			index := 0
 			for stream.Next() {
+				assert.Equal(expectedOutput[index], stream.Value())
+				index++
+			}
+
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+
+	{
+		var (
+			input          = []interface{}{1, 2, 3}
+			expectedOutput = []interface{}{2, 4, 6}
+			mapFn          = func(x interface{}) interface{} { return x.(int) * 2 }
+		)
+
+		t.Run("stream map does not proceed until last value is read", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.Map(mapFn)
+
+			index := 0
+			for stream.Next() {
+				stream.Next()
 				assert.Equal(expectedOutput[index], stream.Value())
 				index++
 			}
@@ -107,6 +159,11 @@ func Test_stream_chunk_by(t *testing.T) {
 
 	var (
 		expectations = []expectation{
+			{
+				nil,
+				nil,
+				func(x interface{}) interface{} { return x },
+			},
 			{
 				[]interface{}{1, 2, 3},
 				[]interface{}{
@@ -169,7 +226,7 @@ func Test_stream_chunk_by(t *testing.T) {
 				stream                     = streamer.NewStream(iterator)
 			)
 
-			stream.ChunkBy(chunkFn)
+			stream = stream.ChunkBy(chunkFn)
 
 			index := 0
 			for stream.Next() {
@@ -180,9 +237,44 @@ func Test_stream_chunk_by(t *testing.T) {
 			assert.Equal(len(expectedOutput), index)
 		})
 	}
+
+	{
+		var (
+			input          = []interface{}{1, 2, 2, 3, 4, 4, 6, 7, 7}
+			expectedOutput = []interface{}{
+				[]interface{}{1},
+				[]interface{}{2, 2},
+				[]interface{}{3},
+				[]interface{}{4, 4},
+				[]interface{}{6},
+				[]interface{}{7, 7},
+			}
+			chunkFn = func(x interface{}) interface{} { return x.(int) % 3 }
+		)
+
+		t.Run("stream chunk by does not proceed until last value is read", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.ChunkBy(chunkFn)
+
+			index := 0
+			for stream.Next() {
+				stream.Next()
+				assert.Equal(expectedOutput[index], stream.Value())
+				index++
+			}
+
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
 }
 
-func Test_chunk_every(t *testing.T) {
+func Test_stream_chunk_every(t *testing.T) {
 	type (
 		expectation struct {
 			input          []interface{}
@@ -193,6 +285,11 @@ func Test_chunk_every(t *testing.T) {
 
 	var (
 		expectations = []expectation{
+			{
+				nil,
+				nil,
+				100,
+			},
 			{
 				[]interface{}{1, 2, 3},
 				[]interface{}{
@@ -238,7 +335,7 @@ func Test_chunk_every(t *testing.T) {
 				stream                     = streamer.NewStream(iterator)
 			)
 
-			stream.ChunkEvery(chunkSize)
+			stream = stream.ChunkEvery(chunkSize)
 
 			index := 0
 			for stream.Next() {
@@ -249,9 +346,41 @@ func Test_chunk_every(t *testing.T) {
 			assert.Equal(len(expectedOutput), index)
 		})
 	}
+
+	{
+		var (
+			input          = []interface{}{1, 2, 3, 4, 5, 6, 7}
+			expectedOutput = []interface{}{
+				[]interface{}{1, 2, 3},
+				[]interface{}{4, 5, 6},
+				[]interface{}{7},
+			}
+			chunkSize = 3
+		)
+
+		t.Run("stream chunk every does not proceed until last value is read", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.ChunkEvery(chunkSize)
+
+			index := 0
+			for stream.Next() {
+				stream.Next()
+				assert.Equal(expectedOutput[index], stream.Value())
+				index++
+			}
+
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
 }
 
-func Test_skip(t *testing.T) {
+func Test_stream_skip(t *testing.T) {
 	type (
 		expectation struct {
 			input          []interface{}
@@ -262,6 +391,11 @@ func Test_skip(t *testing.T) {
 
 	var (
 		expectations = []expectation{
+			{
+				nil,
+				nil,
+				100,
+			},
 			{
 				[]interface{}{1, 2, 3},
 				[]interface{}{1, 2, 3},
@@ -305,7 +439,7 @@ func Test_skip(t *testing.T) {
 				stream                     = streamer.NewStream(iterator)
 			)
 
-			stream.Skip(skipCount)
+			stream = stream.Skip(skipCount)
 
 			index := 0
 			for stream.Next() {
@@ -318,7 +452,7 @@ func Test_skip(t *testing.T) {
 	}
 }
 
-func Test_skip_while(t *testing.T) {
+func Test_stream_skip_while(t *testing.T) {
 	type (
 		expectation struct {
 			input          []interface{}
@@ -329,6 +463,11 @@ func Test_skip_while(t *testing.T) {
 
 	var (
 		expectations = []expectation{
+			{
+				nil,
+				nil,
+				func(elem interface{}) bool { return false },
+			},
 			{
 				[]interface{}{1, 2, 3, 3, 4, 4},
 				[]interface{}{1, 2, 3, 3, 4, 4},
@@ -367,7 +506,7 @@ func Test_skip_while(t *testing.T) {
 				stream                     = streamer.NewStream(iterator)
 			)
 
-			stream.SkipWhile(skipFn)
+			stream = stream.SkipWhile(skipFn)
 
 			index := 0
 			for stream.Next() {
@@ -380,4 +519,352 @@ func Test_skip_while(t *testing.T) {
 			assert.Equal(len(expectedOutput), index)
 		})
 	}
+}
+
+func Test_stream_filter(t *testing.T) {
+	type (
+		expectation struct {
+			input          []interface{}
+			expectedOutput []interface{}
+			filterFn       func(interface{}) bool
+		}
+	)
+
+	var (
+		expectations = []expectation{
+			{
+				nil,
+				nil,
+				func(elem interface{}) bool { return true },
+			},
+			{
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				func(elem interface{}) bool { return true },
+			},
+			{
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				[]interface{}{},
+				func(elem interface{}) bool { return false },
+			},
+			{
+				[]interface{}{1, 3, 11, 9, 21, 17},
+				[]interface{}{3, 9, 21},
+				func(elem interface{}) bool { return elem.(int)%3 == 0 },
+			},
+			{
+				[]interface{}{2, 2, 2, 4, 4, 3, 9, 8},
+				[]interface{}{4, 4, 8},
+				func(elem interface{}) bool { return elem.(int)%4 == 0 },
+			},
+		}
+	)
+
+	for i, exp := range expectations {
+		var (
+			input          = exp.input
+			expectedOutput = exp.expectedOutput
+			filterFn       = exp.filterFn
+		)
+
+		t.Run(fmt.Sprintf("stream filter, test case %v", i+1), func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.Filter(filterFn)
+
+			index := 0
+			for stream.Next() {
+				if len(expectedOutput) > index {
+					assert.Equal(expectedOutput[index], stream.Value())
+					index++
+				}
+			}
+
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+
+	{
+		var (
+			input          = []interface{}{2, 2, 2, 4, 4, 3, 9, 8}
+			expectedOutput = []interface{}{4, 4, 8}
+			filterFn       = func(elem interface{}) bool { return elem.(int)%4 == 0 }
+		)
+
+		t.Run("stream filter, Next() returns true as long as Value() is not called", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.Filter(filterFn)
+
+			stream.Next()
+
+			assert.True(stream.Next())
+
+			index := 0
+			for stream.Next() {
+				if len(expectedOutput) > index {
+					assert.Equal(expectedOutput[index], stream.Value())
+					index++
+				}
+			}
+
+			assert.False(stream.Next())
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+}
+
+func Test_stream_take(t *testing.T) {
+	type (
+		expectation struct {
+			input          []interface{}
+			expectedOutput []interface{}
+			takeCount      int
+		}
+	)
+
+	var (
+		expectations = []expectation{
+			{
+				[]interface{}{1, 2, 3},
+				[]interface{}{1},
+				1,
+			},
+			{
+				[]interface{}{1, 2, 3},
+				[]interface{}{1, 2},
+				2,
+			},
+			{
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				[]interface{}{1, 2, 3},
+				3,
+			},
+			{
+				[]interface{}{},
+				[]interface{}{},
+				4,
+			},
+			{
+				[]interface{}{1, 2},
+				[]interface{}{1, 2},
+				4,
+			},
+		}
+	)
+
+	for i, exp := range expectations {
+		var (
+			input          = exp.input
+			expectedOutput = exp.expectedOutput
+			takeCount      = exp.takeCount
+		)
+
+		t.Run(fmt.Sprintf("stream take, test case %v", i+1), func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.Take(takeCount)
+
+			index := 0
+			for stream.Next() {
+				assert.Equal(expectedOutput[index], stream.Value())
+				index++
+			}
+
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+
+	{
+		var (
+			input          = []interface{}{1, 2, 3}
+			expectedOutput = []interface{}{1}
+			takeCount      = 1
+		)
+
+		t.Run("stream take, Next() returns true as long as Value() is not called", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.Take(takeCount)
+
+			stream.Next()
+
+			assert.True(stream.Next())
+
+			index := 0
+			for stream.Next() {
+				if len(expectedOutput) > index {
+					assert.Equal(expectedOutput[index], stream.Value())
+					index++
+				}
+			}
+
+			assert.False(stream.Next())
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+}
+
+func Test_stream_take_while(t *testing.T) {
+	type (
+		expectation struct {
+			input          []interface{}
+			expectedOutput []interface{}
+			takeFn         func(interface{}) bool
+		}
+	)
+
+	var (
+		expectations = []expectation{
+			{
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				func(elem interface{}) bool { return true },
+			},
+			{
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				[]interface{}{},
+				func(elem interface{}) bool { return false },
+			},
+			{
+				[]interface{}{1, 2, 3, 3, 4, 4},
+				[]interface{}{1, 2, 3, 3},
+				func(elem interface{}) bool { return elem.(int) < 4 },
+			},
+			{
+				[]interface{}{2, 2, 2, 4, 4, 3, 9},
+				[]interface{}{2, 2, 2, 4, 4},
+				func(elem interface{}) bool { return elem.(int)%2 == 0 },
+			},
+		}
+	)
+
+	for i, exp := range expectations {
+		var (
+			input          = exp.input
+			expectedOutput = exp.expectedOutput
+			takeFn         = exp.takeFn
+		)
+
+		t.Run(fmt.Sprintf("stream take while, test case %v", i+1), func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.TakeWhile(takeFn)
+
+			index := 0
+			for stream.Next() {
+				if len(expectedOutput) > index {
+					assert.Equal(expectedOutput[index], stream.Value())
+					index++
+				}
+			}
+
+			assert.False(stream.Next())
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+
+	{
+		var (
+			input          = []interface{}{2, 2, 2, 4, 4, 3, 9}
+			expectedOutput = []interface{}{2, 2, 2, 4, 4}
+			takeFn         = func(elem interface{}) bool { return elem.(int)%2 == 0 }
+		)
+
+		t.Run("stream take while, Next() returns true as long as Value() is not called", func(t *testing.T) {
+			var (
+				assert = assert.New(t)
+
+				iterator streamer.Iterator = streamer.NewSliceIterator(input...)
+				stream                     = streamer.NewStream(iterator)
+			)
+
+			stream = stream.TakeWhile(takeFn)
+
+			stream.Next()
+
+			assert.True(stream.Next())
+
+			index := 0
+			for stream.Next() {
+				if len(expectedOutput) > index {
+					assert.Equal(expectedOutput[index], stream.Value())
+					index++
+				}
+			}
+
+			assert.False(stream.Next())
+			assert.Equal(len(expectedOutput), index)
+		})
+	}
+}
+
+func Test_chain_calls(t *testing.T) {
+	var (
+		assert = assert.New(t)
+
+		input          []interface{}
+		expectedOutput []interface{}
+	)
+
+	for i := 1; i <= 100; i++ {
+		input = append(input, i)
+	}
+
+	for i := 1; i <= 99; i++ {
+		v := i + 100
+		if v%11 != 0 {
+			continue
+		}
+		if v <= 121 {
+			continue
+		}
+		expectedOutput = append(expectedOutput, v)
+	}
+
+	iterator := streamer.NewSliceIterator(input...)
+	stream := streamer.NewStream(iterator)
+
+	stream = stream.
+		Map(func(v interface{}) interface{} { return v.(int) + 100 }).
+		Take(99).
+		Filter(func(v interface{}) bool { return v.(int)%11 == 0 }).
+		Skip(1).
+		SkipWhile(func(v interface{}) bool { return v.(int) <= 121 })
+
+	index := 0
+	for stream.Next() {
+		item := stream.Value()
+		if len(expectedOutput) > index {
+			assert.Equal(expectedOutput[index], item)
+			index++
+		}
+	}
+
+	assert.Equal(len(expectedOutput), index)
 }
